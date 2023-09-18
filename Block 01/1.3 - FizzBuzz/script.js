c95b27a08843acfdcbae54faae4b06182c563242
isaaclins <isaac.lins07@gmail.com>
2023-09-18 13:29:43 +0200

    fixed fizzbuzz and added template

--- Block 01/1.3 - FizzBuzz/script.js
+++ Block 01/1.3 - FizzBuzz/script.js
@@ -1,11 +1,11 @@
 function fizzbuzz() {
   for (let i = 0; i < 101; i++) {
     if (i % 15 == 0) {
-      console.log('fizzbuzz: ' + i + '\n (' + i / 15 + '/15)');
+      console.log('fizzbuzz: ' + i + '\n (' + i / 15 + ' * 15)');
     } else if (i % 3 == 0) {
-      console.log('fizz: ' + i + '\n (' + i / 3 + '/3)');
+      console.log('fizz: ' + i + '\n (' + i / 3 + ' * 3)');
     } else if (i % 5 == 0) {
-      console.log('buzz:' + i + '\n (' + i / 5 + '/5)');
+      console.log('buzz:' + i + '\n (' + i / 5 + ' * 5)');
     } else {
       console.log(i);
     }
