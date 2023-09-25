b0380f95b2cac946f34aeb27dac877e1f60e3df3
isaaclins <isaac.lins07@gmail.com>
2023-09-25 08:57:13 +0200

    added a button for every ease/linear etc

--- Block 05/5.1 - Mit CSS Transitions arbeiten/script.js
+++ Block 05/5.1 - Mit CSS Transitions arbeiten/script.js
@@ -1,6 +1,30 @@
-const button = document.querySelector('#button');
 const square = document.querySelector('#square');
 
-button.onclick = () => {
-  square.classList.toggle('square-end');
+const applyTransition = (timingFunction) => {
+  square.style.transition = `all 1.5s ${timingFunction}`;
 };
+
+document.querySelector('#button').addEventListener('click', () => {
+  applyTransition('ease');
+  square.classList.toggle('square-end');
+});
+
+document.querySelector('#button-linear').addEventListener('click', () => {
+  applyTransition('linear');
+  square.classList.toggle('square-end');
+});
+
+document.querySelector('#button-ease-in').addEventListener('click', () => {
+  applyTransition('ease-in');
+  square.classList.toggle('square-end');
+});
+
+document.querySelector('#button-ease-out').addEventListener('click', () => {
+  applyTransition('ease-out');
+  square.classList.toggle('square-end');
+});
+
+document.querySelector('#button-ease-in-out').addEventListener('click', () => {
+  applyTransition('ease-in-out');
+  square.classList.toggle('square-end');
+});
