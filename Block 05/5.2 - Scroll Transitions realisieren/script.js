6e5adac2886816160d9585819b0f154ff0b22a2c
isaaclins <isaac.lins07@gmail.com>
2023-09-25 09:35:00 +0200

    finished 5.2

--- Block 05/5.2 - Scroll Transitions realisieren/script.js
+++ Block 05/5.2 - Scroll Transitions realisieren/script.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("main > h2, main > img, main > p");
 
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
   function checkScroll() {
     sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      if (sectionTop < windowHeight - 100) {
+      if (isInViewport(section)) {
         section.classList.add("fade-in-active");
         section.classList.add("slide-in-left-active");
         section.classList.add("swirl-in-active");
-      }
-      if (sectionTop > windowHeight - 100) {
+      } else {
         section.classList.remove("fade-in-active");
         section.classList.remove("slide-in-left-active");
         section.classList.remove("swirl-in-active");
       }
-
     });
   }
 
